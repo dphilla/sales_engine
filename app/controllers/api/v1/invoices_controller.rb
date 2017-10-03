1f8fc7a386407646f7fa6b6a03529b5acc0ff879
Daniel Phillips <danieledward.phillips@gmail.com>
2017-10-03 08:26:51 -0600

    add invoice show and index

---
+++ app/controllers/api/v1/invoices_controller.rb
@@ -0,0 +1,25 @@
+module Api # a more common namespacing technique (as opposed to scope resolution ops)
+  module V1
+    class InvoicesController < ApplicationController
+
+      def index
+        render json: Invoice.all
+      end
+
+      def show
+        render json: Invoice.find(params[:id])
+      end
+
+
+
+      private
+
+      def invoice_params
+
+      end
+
+
+
+    end
+  end
+end
--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -3,11 +3,28 @@ require 'rails_helper'
 describe "invoice request" do
    it "sends a list of invoices" do
 
-     create_list(:invoices, 3)
+     create_list(:invoice, 3)
 
-     get '/api/v1/invoices.json'
+     get '/api/v1/invoices'
 
      expect(response).to be_success
+
+     invoices = JSON.parse(response.body)
+
+     expect(invoices.count).to eq(3)
+
+   end
+
+   it "sends back one invoice" do
+
+     create_list(:invoice, 3)
+
+     get "/api/v1/invoices/#{Invoice.last.id}"
+
+     invoice = JSON.parse(response.body)
+
+     expect(invoice["id"]).to eq(Invoice.last.id)
+
 
    end
 end
