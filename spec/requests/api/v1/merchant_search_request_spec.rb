1003938e8f2c083574f08adf0e630adcb0d38f65
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-10-03 17:21:29 -0600

    going to reseed to account for names separated by commas

--- spec/requests/api/v1/merchant_search_request_spec.rb
+++ spec/requests/api/v1/merchant_search_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Merchants API" do
-  it "finds by id" do
+  xit "finds by id" do
     merch1, merch2, merch3 = create_list(:merchant, 3)
 
     get "/api/v1/merchants/find?id=#{merch1.id}"
@@ -12,7 +12,7 @@ describe "Merchants API" do
     expect(merchant["id"]).to eq(merch1.id)
   end
 
-  it "finds by name" do
+  xit "finds by name" do
     merch1, merch2, merch3 = create_list(:merchant, 3)
 
     get "/api/v1/merchants/find?name=#{merch1.name}"
@@ -23,8 +23,15 @@ describe "Merchants API" do
     expect(merchant["name"]).to eq(merch1.name)
   end
 
-  it "finds by created at date" do
+  xit "finds by created at date" do
+    merch1, merch2, merch3 = create_list(:merchant, 3)
 
+    get "/api/v1/merchants/find?created_at=#{merch1.created_at}"
+# binding.pry
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["created_at"]).to eq(merch1.created_at)
   end
 
   it "finds by updated at date" do
