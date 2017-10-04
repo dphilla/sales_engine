e03c1e6ba57d9d73375e97ab1c6fa98e570c5514
Daniel Phillips <danieledward.phillips@gmail.com>
2017-10-04 08:35:26 -0600

    start model AR methods

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,12 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :customers, through: :invoices
   has_many :items
+
+
+  def self.top_by_revenue(quantity)
+    Merchant.find_by_sql("SELECT * FROM merchants
+                         INNER JOIN invoices
+                         ON merchants.id=merchant_id
+                         INNER JOIN... ")
+  end
 end
