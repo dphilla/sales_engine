046563eb4984c9aa2c17e1b5bd75c62422ae5783
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-10-05 17:29:15 -0600

    customer favorite merchant bi spec pass

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,5 +2,26 @@ class Customer < ApplicationRecord
   has_many :invoices
   has_many :merchants, through: :invoices
 
+  #
+  # def self.favorite_merchant(id)
+  #   Merchant.select("merchants.*, count('transactions.result')")
+  #     .joins(invoices: [:transactions])
+  #     .merge(Transaction.successful)
+  #     .where('invoices.customer_id = ?', id)
+  #     .group(:id)
+  #     .order("count DESC")
+  #     .limit(1)
+  # end
 
+
+    def favorite_merchant
+    m =   Merchant.select("merchants.*, count('transactions.result')")
+        .joins(invoices: [:transactions])
+        .merge(Transaction.successful)
+        .where('invoices.customer_id = ?', self.id)
+        .group(:id)
+        .order("count DESC")
+        .limit(1)
+        m[0]
+    end
 end
