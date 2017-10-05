e1213efd17cb488544fadb0429cefceaf9ce9ca0
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-10-05 10:28:04 -0600

    find one for invoice item request pass

--- spec/requests/api/v1/invoice_item_search_request_spec.rb
+++ spec/requests/api/v1/invoice_item_search_request_spec.rb
@@ -13,15 +13,59 @@ describe "InvoiceItems API" do
   end
 
   it "finds by item_id" do
+    ii1 = create(:invoice_item)
 
+    get "/api/v1/invoice_items/find?item_id=#{ii1.item_id}"
+
+    result = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(result["item_id"]).to eq(ii1.item_id)
   end
 
   it "finds by invoice_id" do
+    ii1 = create(:invoice_item)
 
+    get "/api/v1/invoice_items/find?invoice_id=#{ii1.invoice_id}"
+
+    result = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(result["invoice_id"]).to eq(ii1.invoice_id)
   end
 
-  it "fins by unit price" do
+  it "finds by unit price" do
+    ii1 = create(:invoice_item)
+    price = (ii1.unit_price / 100).to_s
 
+    get "/api/v1/invoice_items/find?unit_price=#{price}"
+
+    result = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(result["id"]).to eq(ii1.id)
+  end
+
+  it "finds by created at" do
+    ii1 = create(:invoice_item, created_at: "Sep 1 2002")
+
+    get "/api/v1/invoice_items/find?created_at=#{ii1.created_at}"
+
+    result = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(result["id"]).to eq(ii1.id)
+  end
+
+  it "finds by updated at" do
+    ii1 = create(:invoice_item, updated_at: "Sep 1 2002")
+
+    get "/api/v1/invoice_items/find?updated_at=#{ii1.updated_at}"
+
+    result = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(result["id"]).to eq(ii1.id)
   end
 
 end
