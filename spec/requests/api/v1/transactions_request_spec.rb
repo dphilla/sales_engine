10b5a70d30d966605f73d2b951bf6500e12be3c9
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-10-04 19:45:25 -0600

    show for transaction request

--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -11,4 +11,15 @@ describe "Transactions API" do
     expect(response).to be_success
     expect(transaction.count).to eq(3)
   end
+
+  it "gets one transaction by id" do
+    create_list(:transaction, 3)
+
+    get "/api/v1/transactions/#{Transaction.last.id}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction["id"]).to eq(Transaction.last.id)
+  end
 end
