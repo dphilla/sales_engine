ef759023512e3b347b04a7e41c07e21b83486470
Daniel Phillips <danieledward.phillips@gmail.com>
2017-10-05 12:51:18 -0600

    take out pry

--- app/controllers/api/v1/customers/transactions_controller.rb
+++ app/controllers/api/v1/customers/transactions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::Customers::TransactionsController < ApplicationController
   def index
     customer = Customer.find(params[:customer_id])
     render json: customer.invoices.customer_transactions
-    require 'pry'; binding.pry
+    #require 'pry'; binding.pry
   end
 
 end
